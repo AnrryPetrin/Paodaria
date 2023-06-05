623c47b5b5956bc6b8f5008b074c13c7be343c6e
AnrryPetrin <108158140+AnrryPetrin@users.noreply.github.com>
2023-06-05 10:03:02 -0300

    elaborando a estrutura do projeto

--- src/main/Main.java
+++ src/main/Main.java
@@ -2,13 +2,13 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package paodaria;
+package main;
 
 /**
  *
  * @author anrry
  */
-public class Paodaria {
+public class Main {
 
     /**
      * @param args the command line arguments
---
+++ src/objetos/Funcionario.java
@@ -0,0 +1,76 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package objetos;
+
+/**
+ *
+ * @author anrry
+ */
+public class Funcionario {
+    private String nome;
+    private String cpf;
+    private String sexo;
+    private String endereço;
+    private String cidade;
+    private String estado;
+
+    public Funcionario(String nome, String cpf, String sexo, String endereço, String cidade, String estado) {
+        this.nome = nome;
+        this.cpf = cpf;
+        this.sexo = sexo;
+        this.endereço = endereço;
+        this.cidade = cidade;
+        this.estado = estado;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+    public String getEndereço() {
+        return endereço;
+    }
+
+    public void setEndereço(String endereço) {
+        this.endereço = endereço;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+    
+}
