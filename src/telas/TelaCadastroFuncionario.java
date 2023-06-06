4ae5ba01470130d0b96fbe377e5358942420bb5d
Murilo <muzbz09@gmail.com>
2023-06-05 22:14:56 -0300

    deu certo jesus

--- src/telas/TelaCadastroFuncionario.java
+++ src/telas/TelaCadastroFuncionario.java
@@ -14,6 +14,7 @@ import conexoes.MySQL;
 public class TelaCadastroFuncionario extends javax.swing.JFrame {
         MySQL conectar = new MySQL();
         Funcionario novoFuncionario = new Funcionario();
+        //100% Jeusus
     /**
      * Creates new form TelaCadastroFuncionario
      */
